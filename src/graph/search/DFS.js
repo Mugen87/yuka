d77243503b3d95815ec2d1b4bf3008648f4be654
Mugen87 <michael.herzog@human-interactive.org>
2018-01-08 21:39:50 +0100

    Added DFS

---
+++ src/graph/search/DFS.js
@@ -0,0 +1,150 @@
+/**
+ * @author Mugen87 / https://github.com/Mugen87
+ */
+
+import { Edge } from '../core/Edge.js';
+
+class DFS {
+
+	constructor( graph, source, target ) {
+
+		this.graph = graph;
+		this.source = source;
+		this.target = target;
+		this.found = false;
+
+		this._route = new Map(); // this holds the route taken to the target
+		this._visited = new Set(); // holds the visited nodes
+
+		this._spanningTree = new Set(); // for debugging purposes
+
+	}
+
+	search() {
+
+		// create a stack(LIFO) of edges, done via an array
+
+		const stack = [];
+		const outgoingEdges = [];
+
+		// create a dummy edge and put on the stack to begin the search
+
+		const startEdge = new Edge( this.source, this.source );
+
+		stack.push( startEdge );
+
+		// while there are edges in the stack keep searching
+
+		while ( stack.length > 0 ) {
+
+			// grab the next edge and remove it from the stack
+
+			const nextEdge = stack.pop();
+
+			// make a note of the parent of the node this edge points to
+
+			this._route.set( nextEdge.to, nextEdge.from );
+
+			// and mark it visited
+
+			this._visited.add( nextEdge.to );
+
+			// expand spanning tree
+
+			if ( nextEdge !== startEdge ) {
+
+				this._spanningTree.add( nextEdge );
+
+			}
+
+			// if the target has been found the method can return success
+
+			if ( nextEdge.to === this.target ) {
+
+				this.found = true;
+
+				return this;
+
+			}
+
+			// determine outgoing edges
+
+			this.graph.getEdgesOfNode( nextEdge.to, outgoingEdges );
+
+			// push the edges leading from the node this edge points to onto the
+			// stack (provided the edge does not point to a previously visited node)
+
+			for ( let edge of outgoingEdges ) {
+
+				if ( this._visited.has( edge.to ) === false ) {
+
+					stack.push( edge );
+
+				}
+
+			}
+
+		}
+
+		this.found = false;
+
+		return this;
+
+	}
+
+	getPath() {
+
+		// array of node indices that comprise the shortest path from the source to the target
+
+		const path = [];
+
+		// just return an empty path if no path to target found or if no target has been specified
+
+		if ( this.found === false || this.target === undefined ) return path;
+
+		// start with the target of the path
+
+		let currentNode = this.target;
+
+		path.push( currentNode );
+
+		// while the current node is not the source node keep processing
+
+		while ( currentNode !== this.source ) {
+
+			// determine the parent of the current node
+
+			currentNode = this._route.get( currentNode );
+
+			// push the new current node at the beginning of the array
+
+			path.unshift( currentNode );
+
+		}
+
+		return path;
+
+	}
+
+	getSearchTree() {
+
+		return [ ...this._spanningTree ];
+
+	}
+
+	clear() {
+
+		this.graph = undefined;
+		this.source = undefined;
+		this.target = undefined;
+		this.found = false;
+
+		this._route.clear();
+		this._visited.clear();
+		this._spanningTree.clear();
+
+	}
+
+}
+
+export { DFS };
--- src/yuka.js
+++ src/yuka.js
@@ -6,6 +6,7 @@ export { Edge } from './graph/core/Edge.js';
 export { Graph } from './graph/core/Graph.js';
 export { NavNode } from './graph/navigation/NavNode.js';
 export { NavEdge } from './graph/navigation/NavEdge.js';
+export { DFS } from './graph/search/DFS.js';
 export { Path } from './steering/Path.js';
 export { Vehicle } from './steering/Vehicle.js';
 export { ArriveBehavior } from './steering/behaviors/ArriveBehavior.js';
