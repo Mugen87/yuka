c2f147f894dc5fda30d00fd37108de134aa3d1ec
robert <robert@DESKTOP-49BK7B2>
2018-08-04 18:57:24 +0200

    Tests: added Tests for Vector3

--- test/unit_test/math/Vector3.tests.js
+++ test/unit_test/math/Vector3.tests.js
@@ -70,5 +70,105 @@ describe( 'Vector3', function () {
 		} );
 
 	} );
+	describe( '#sub', function () {
+
+		it( 'should return zero vector when same vector is subtracted', function () {
+
+			const v1 = new Vector3( x, y, z );
+			const v2 = new Vector3();
+
+			v1.sub( v1 );
+
+			expect( v1 ).to.deep.equal( { x: v2.x, y: v2.y, z: v2.z } );
+
+		} );
+
+	} );
+
+	describe( '#subScalar()', function () {
+
+		it( 'should return an unaltered vector when subbing 0', function () {
+
+			const v1 = new Vector3( x, y, z );
+			v1.subScalar( 0 );
+
+			expect( v1 ).to.deep.equal( { x: x, y: y, z: z } );
+
+		} );
+
+
+	} );
+
+
+	describe( '#subVectors()', function () {
+
+		it( 'should return the same vector when a zero vector is subbed', function ( ) {
+
+			const v1 = new Vector3( x, y, z );
+			const v0 = new Vector3();
+			const v2 = new Vector3().subVectors( v1, v0 );
+
+			expect( v2 ).to.deep.equal( { x: v1.x, y: v1.y, z: v1.z } );
+
+		} );
+
+
+	} );
+
+	describe( '#multiply()', function () {
+
+		it( 'should return product of vector multiplication', function () {
+
+        	const v1 = new Vector3( x, y, z );
+        	const v2 = new Vector3( x, y, z );
+
+        	v1.multiply( v2 );
+
+        	expect( v1 ).to.deep.equal( { x: Math.pow( v2.x, 2 ), y: Math.pow( v2.y, 2 ), z: Math.pow( v2.z, 2 ) } );
+
+		} );
+
+	} );
+
+	describe( '#multiplyScalar()', function () {
+
+		it( 'should return unaltered vector when multiplying with 1', function () {
+
+			const v1 = new Vector3( x, y, z );
+			v1.multiplyScalar( 1 );
+
+			expect( v1 ).to.deep.equal( { x: x, y: y, z: z } );
+
+		} );
+
+	} );
+
+	describe( '#multiplyVectors()', function () {
+
+		it( 'should return product of vector multiplication', function () {
+
+			const v1 = new Vector3( x, y, z );
+			const v2 = new Vector3().multiplyVectors( v1, v1 );
+
+			expect( v2 ).to.deep.equal( { x: Math.pow( v1.x, 2 ), y: Math.pow( v1.y, 2 ), z: Math.pow( v1.z, 2 ) } );
+
+		} );
+
+	} );
+
+	describe( '#divide()', function () {
+
+		it( 'should return unaltered vector when dviding with vector1', function () {
+
+        	const v1 = new Vector3( x, y, z );
+        	const v2 = new Vector3( 1, 1, 1 );
+
+        	v1.divide( v2 );
+
+			expect( v1 ).to.deep.equal( { x: x, y: y, z: z } );
+
+		} );
+
+	} );
 
 } );
