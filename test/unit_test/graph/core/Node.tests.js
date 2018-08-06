60749f383a8406fe160922ef017379b775d88d36
Mugen87 <michael.herzog@human-interactive.org>
2018-08-06 11:49:16 +0200

    Added unit tests for Node and Edge classes

---
+++ test/unit_test/graph/core/Node.tests.js
@@ -0,0 +1,31 @@
+/**
+ * @author Mugen87 / https://github.com/Mugen87
+ *
+ */
+
+const expect = require( 'chai' ).expect;
+const YUKA = require( '../../../../build/yuka.js' );
+
+const Node = YUKA.Node;
+
+describe( 'Node', function () {
+
+	describe( '#constructor()', function () {
+
+		it( 'should create an object with correct default values', function () {
+
+			const node = new Node();
+			expect( node ).to.have.a.property( 'index' );
+
+		} );
+
+		it( 'should apply the parameters to the new object', function () {
+
+			const node = new Node( 1 );
+			expect( node.index ).to.equal( 1 );
+
+		} );
+
+	} );
+
+} );
