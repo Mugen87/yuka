4fd7e4ef5fecec90d0df36e9c248b0e09ca2a1ee
Mugen87 <michael.herzog@human-interactive.org>
2018-09-08 22:14:01 +0200

    Added basic test for NavMeshLoader

---
+++ test/unit_test/graph/navmesh/NavMeshLoader.js
@@ -0,0 +1,39 @@
+/**
+ * @author Mugen87 / https://github.com/Mugen87
+ */
+
+const expect = require( 'chai' ).expect;
+const fetch = require( 'node-fetch' );
+const encoding = require( 'text-encoding' );
+
+global.fetch = fetch;
+global.TextDecoder = encoding.TextDecoder;
+
+const YUKA = require( '../../../../build/yuka.js' );
+
+const NavMeshLoader = YUKA.NavMeshLoader;
+const NavMesh = YUKA.NavMesh;
+
+//
+
+describe( 'NavMeshLoader', function () {
+
+	describe( '#load()', function () {
+
+		it( 'should return a promise that resolves if the navigation mesh with the given URL is successfully loaded', function ( done ) {
+
+			const loader = new NavMeshLoader();
+
+			loader.load( 'https://raw.githubusercontent.com/Mugen87/yuka/master/test/assets/navmesh/basic/navmesh.gltf' ).then( ( navMesh ) => {
+
+				expect( navMesh ).is.an.instanceof( NavMesh );
+
+				done();
+
+			} );
+
+		} );
+
+	} );
+
+} );
