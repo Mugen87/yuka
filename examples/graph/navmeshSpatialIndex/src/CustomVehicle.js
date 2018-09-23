d50b4c2e2dba027e6286c35ac1edbbfccbc84545
Mugen87 <michael.herzog@human-interactive.org>
2018-09-23 17:52:19 +0200

    Examples: Made navMesh example with spatial index more complex

---
+++ examples/graph/navmeshSpatialIndex/src/CustomVehicle.js
@@ -0,0 +1,44 @@
+/**
+ * @author Mugen87 / https://github.com/Mugen87
+ */
+
+import { Vehicle } from '../../../../build/yuka.module.js';
+
+class CustomVehicle extends Vehicle {
+
+	constructor() {
+
+		super();
+
+		this.navMesh = null;
+
+		this.currentRegion = null;
+		this.fromRegion = null;
+		this.toRegion = null;
+
+	}
+
+	update( delta ) {
+
+		super.update( delta );
+
+		// this code is used to adjust the height of the entity according to its current region
+
+		const currentRegion = this.navMesh.getRegionForPoint( this.position, 1 );
+
+		if ( currentRegion !== null ) {
+
+			this.currentRegion = currentRegion;
+
+			const distance = this.currentRegion.plane.distanceToPoint( this.position );
+
+			this.position.y -= distance * 0.2;
+
+		}
+
+	}
+
+
+}
+
+export { CustomVehicle };
