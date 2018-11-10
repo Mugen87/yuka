1d86cc0c9508939e6d990a509386eb796d79a9fd
Mugen87 <michael.herzog@human-interactive.org>
2018-11-10 18:24:17 +0100

    Added license to build files

--- config/rollup/config.js
+++ config/rollup/config.js
@@ -1,5 +1,16 @@
+const fs = require( 'fs' );
+const commenting = require( 'commenting' );
+
 export default {
 	input: 'src/yuka.js',
+	plugins: [ {
+		banner() {
+
+			const text = fs.readFileSync( 'LICENSE', 'utf8' );
+			return commenting( '@license\n' + text, { extension: '.js' } );
+
+		}
+	} ],
 	output: [
 		{
 			format: 'umd',
