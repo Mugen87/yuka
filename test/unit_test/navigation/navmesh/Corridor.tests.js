d1684c427655988a6c41258c72d15aa39edd315d
Mugen87 <michael.herzog@human-interactive.org>
2019-01-30 14:56:47 +0100

    Tests: Check edge case in Corridor

--- test/unit_test/navigation/navmesh/Corridor.tests.js
+++ test/unit_test/navigation/navmesh/Corridor.tests.js
@@ -67,6 +67,31 @@ describe( 'Corridor', function () {
 
 		} );
 
+		it( 'should generate the a path with unique waypoints', function () {
+
+			const corridor = new Corridor();
+
+			const from = new Vector3();
+			const to = new Vector3( 10, 0, 0 );
+
+			corridor.push( from, from );
+			corridor.push( new Vector3( 3, 0, - 5 ), new Vector3( 3, 0, - 7 ) );
+			corridor.push( new Vector3( 3, 0, - 5 ), new Vector3( 3.5, 0, - 7 ) );
+			corridor.push( new Vector3( 3, 0, - 5 ), new Vector3( 3.75, 0, - 7 ) );
+			corridor.push( new Vector3( 6, 0, 7 ), new Vector3( 6, 0, 5 ) );
+			corridor.push( new Vector3( 8, 0, - 10 ), new Vector3( 8, 0, 10 ) );
+			corridor.push( to, to );
+
+			const path = corridor.generate();
+
+			expect( path ).to.have.lengthOf( 4 );
+			expect( path[ 0 ] ).to.deep.equal( new Vector3() );
+			expect( path[ 1 ] ).to.deep.equal( new Vector3( 3, 0, - 5 ) ); // (3, 0, - 5) should be only once in the array of waypoints
+			expect( path[ 2 ] ).to.deep.equal( new Vector3( 6, 0, 5 ) );
+			expect( path[ 3 ] ).to.deep.equal( new Vector3( 10, 0, 0 ) );
+
+		} );
+
 	} );
 
 } );
