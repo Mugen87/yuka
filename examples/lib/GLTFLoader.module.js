30c43240153cd835de670f88e1cc0627a0790b20
Mugen87 <michael.herzog@human-interactive.org>
2019-03-07 12:06:15 +0100

    Quaternion: Added .extractRotationFromMatrix()

--- examples/lib/GLTFLoader.module.js
+++ examples/lib/GLTFLoader.module.js
@@ -383,13 +383,13 @@ GLTFLightsExtension.prototype.loadLight = function ( lightIndex ) {
  *
  * PR: https://github.com/KhronosGroup/glTF/pull/1163
  */
-function GLTFMaterialsUnlitExtension( json ) {
+function GLTFMaterialsUnlitExtension( /* json */ ) {
 
 	this.name = EXTENSIONS.KHR_MATERIALS_UNLIT;
 
 }
 
-GLTFMaterialsUnlitExtension.prototype.getMaterialType = function ( material ) {
+GLTFMaterialsUnlitExtension.prototype.getMaterialType = function ( /*  material */ ) {
 
 	return THREE.MeshBasicMaterial;
 
@@ -429,7 +429,7 @@ GLTFMaterialsUnlitExtension.prototype.extendParams = function ( materialParams, 
 
 /* BINARY EXTENSION */
 
-var BINARY_EXTENSION_BUFFER_NAME = 'binary_glTF';
+// var BINARY_EXTENSION_BUFFER_NAME = 'binary_glTF';
 var BINARY_EXTENSION_HEADER_MAGIC = 'glTF';
 var BINARY_EXTENSION_HEADER_LENGTH = 12;
 var BINARY_EXTENSION_CHUNK_TYPES = { JSON: 0x4E4F534A, BIN: 0x004E4942 };
@@ -577,7 +577,7 @@ GLTFDracoMeshCompressionExtension.prototype.decodePrimitive = function ( primiti
  *
  * Specification:
  */
-function GLTFTextureTransformExtension( json ) {
+function GLTFTextureTransformExtension( /* json */ ) {
 
 	this.name = EXTENSIONS.KHR_TEXTURE_TRANSFORM;
 
@@ -863,7 +863,7 @@ function GLTFMaterialsPbrSpecularGlossinessExtension() {
 		},
 
 		// Here's based on refreshUniformsCommon() and refreshUniformsStandard() in WebGLRenderer.
-		refreshUniforms: function ( renderer, scene, camera, geometry, material, group ) {
+		refreshUniforms: function ( renderer, scene, camera, geometry, material /* , group */ ) {
 
 			if ( material.isGLTFSpecularGlossinessMaterial !== true ) {
 
@@ -1111,16 +1111,16 @@ var WEBGL_CONSTANTS = {
 	UNSIGNED_SHORT: 5123
 };
 
-var WEBGL_TYPE = {
-	5126: Number,
-	//35674: THREE.Matrix2,
-	35675: THREE.Matrix3,
-	35676: THREE.Matrix4,
-	35664: THREE.Vector2,
-	35665: THREE.Vector3,
-	35666: THREE.Vector4,
-	35678: THREE.Texture
-};
+// var WEBGL_TYPE = {
+// 	5126: Number,
+// 	//35674: THREE.Matrix2,
+// 	35675: THREE.Matrix3,
+// 	35676: THREE.Matrix4,
+// 	35664: THREE.Vector2,
+// 	35665: THREE.Vector3,
+// 	35666: THREE.Vector4,
+// 	35678: THREE.Texture
+// };
 
 var WEBGL_COMPONENT_TYPES = {
 	5120: Int8Array,
@@ -1146,47 +1146,47 @@ var WEBGL_WRAPPINGS = {
 	10497: THREE.RepeatWrapping
 };
 
-var WEBGL_SIDES = {
-	1028: THREE.BackSide, // Culling front
-	1029: THREE.FrontSide // Culling back
-	//1032: THREE.NoSide   // Culling front and back, what to do?
-};
+// var WEBGL_SIDES = {
+// 	1028: THREE.BackSide, // Culling front
+// 	1029: THREE.FrontSide // Culling back
+// 	//1032: THREE.NoSide   // Culling front and back, what to do?
+// };
 
-var WEBGL_DEPTH_FUNCS = {
-	512: THREE.NeverDepth,
-	513: THREE.LessDepth,
-	514: THREE.EqualDepth,
-	515: THREE.LessEqualDepth,
-	516: THREE.GreaterEqualDepth,
-	517: THREE.NotEqualDepth,
-	518: THREE.GreaterEqualDepth,
-	519: THREE.AlwaysDepth
-};
+// var WEBGL_DEPTH_FUNCS = {
+// 	512: THREE.NeverDepth,
+// 	513: THREE.LessDepth,
+// 	514: THREE.EqualDepth,
+// 	515: THREE.LessEqualDepth,
+// 	516: THREE.GreaterEqualDepth,
+// 	517: THREE.NotEqualDepth,
+// 	518: THREE.GreaterEqualDepth,
+// 	519: THREE.AlwaysDepth
+// };
 
-var WEBGL_BLEND_EQUATIONS = {
-	32774: THREE.AddEquation,
-	32778: THREE.SubtractEquation,
-	32779: THREE.ReverseSubtractEquation
-};
+// var WEBGL_BLEND_EQUATIONS = {
+// 	32774: THREE.AddEquation,
+// 	32778: THREE.SubtractEquation,
+// 	32779: THREE.ReverseSubtractEquation
+// };
 
-var WEBGL_BLEND_FUNCS = {
-	0: THREE.ZeroFactor,
-	1: THREE.OneFactor,
-	768: THREE.SrcColorFactor,
-	769: THREE.OneMinusSrcColorFactor,
-	770: THREE.SrcAlphaFactor,
-	771: THREE.OneMinusSrcAlphaFactor,
-	772: THREE.DstAlphaFactor,
-	773: THREE.OneMinusDstAlphaFactor,
-	774: THREE.DstColorFactor,
-	775: THREE.OneMinusDstColorFactor,
-	776: THREE.SrcAlphaSaturateFactor
-	// The followings are not supported by Three.js yet
-	//32769: CONSTANT_COLOR,
-	//32770: ONE_MINUS_CONSTANT_COLOR,
-	//32771: CONSTANT_ALPHA,
-	//32772: ONE_MINUS_CONSTANT_COLOR
-};
+// var WEBGL_BLEND_FUNCS = {
+// 	0: THREE.ZeroFactor,
+// 	1: THREE.OneFactor,
+// 	768: THREE.SrcColorFactor,
+// 	769: THREE.OneMinusSrcColorFactor,
+// 	770: THREE.SrcAlphaFactor,
+// 	771: THREE.OneMinusSrcAlphaFactor,
+// 	772: THREE.DstAlphaFactor,
+// 	773: THREE.OneMinusDstAlphaFactor,
+// 	774: THREE.DstColorFactor,
+// 	775: THREE.OneMinusDstColorFactor,
+// 	776: THREE.SrcAlphaSaturateFactor
+// 	// The followings are not supported by Three.js yet
+// 	//32769: CONSTANT_COLOR,
+// 	//32770: ONE_MINUS_CONSTANT_COLOR,
+// 	//32771: CONSTANT_ALPHA,
+// 	//32772: ONE_MINUS_CONSTANT_COLOR
+// };
 
 var WEBGL_TYPE_SIZES = {
 	'SCALAR': 1,
@@ -1225,14 +1225,14 @@ var INTERPOLATION = {
 	STEP: THREE.InterpolateDiscrete
 };
 
-var STATES_ENABLES = {
-	2884: 'CULL_FACE',
-	2929: 'DEPTH_TEST',
-	3042: 'BLEND',
-	3089: 'SCISSOR_TEST',
-	32823: 'POLYGON_OFFSET_FILL',
-	32926: 'SAMPLE_ALPHA_TO_COVERAGE'
-};
+// var STATES_ENABLES = {
+// 	2884: 'CULL_FACE',
+// 	2929: 'DEPTH_TEST',
+// 	3042: 'BLEND',
+// 	3089: 'SCISSOR_TEST',
+// 	32823: 'POLYGON_OFFSET_FILL',
+// 	32926: 'SAMPLE_ALPHA_TO_COVERAGE'
+// };
 
 var ALPHA_MODES = {
 	OPAQUE: 'OPAQUE',
