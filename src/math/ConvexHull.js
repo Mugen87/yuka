c4ac988852c4546bb9d295eca7a401cdece7708f
Mugen87 <michael.herzog@human-interactive.org>
2019-04-08 16:50:22 +0200

    ConvexHull: Initial commit.

---
+++ src/math/ConvexHull.js
@@ -0,0 +1,238 @@
+import { Logger } from '../core/Logger.js';
+
+/**
+* Class representing a convex hull.
+*
+* @author {@link https://github.com/Mugen87|Mugen87}
+*/
+class ConvexHull {
+
+	constructor() {
+
+		this.faces = [];
+		this.vertices = [];
+
+		//
+
+		this._tolerance = - 1;
+
+		this._assigned = new VertexList();
+		this._unassigned = new VertexList();
+
+		this._newFaces = [];
+
+	}
+
+	fromPoints( points ) {
+
+		if ( points.length < 4 ) {
+
+			Logger.error( 'YUKA.ConvexHull: The given points array needs at least four points' );
+			return this;
+
+		}
+
+		for ( let i = 0, l = points.length; i < l; i ++ ) {
+
+			this.vertices.push( new Vertex( points[ i ] ) );
+
+		}
+
+		this._compute();
+
+		return this;
+
+	}
+
+	makeEmpty() {
+
+		this.faces = [];
+		this.vertices = [];
+
+		return this;
+
+	}
+
+	// private API
+
+	_compute() {
+
+		return this;
+
+	}
+
+}
+
+//
+
+class Vertex {
+
+	constructor( point ) {
+
+		this.point = point;
+		this.prev = null;
+		this.next = null;
+		this.face = null; // the face that is able to see this vertex
+
+	}
+
+}
+
+class VertexList {
+
+	constructor() {
+
+		this.head = null;
+		this.tail = null;
+
+	}
+
+	first() {
+
+		return this.head;
+
+	}
+
+	last() {
+
+		return this.tail;
+
+	}
+
+	clear() {
+
+		this.head = this.tail = null;
+
+		return this;
+
+	}
+
+	insertBefore( target, vertex ) {
+
+		vertex.prev = target.prev;
+		vertex.next = target;
+
+		if ( vertex.prev === null ) {
+
+			this.head = vertex;
+
+		} else {
+
+			vertex.prev.next = vertex;
+
+		}
+
+		target.prev = vertex;
+
+		return this;
+
+	}
+
+	append( vertex ) {
+
+		if ( this.head === null ) {
+
+			this.head = vertex;
+
+		} else {
+
+			this.tail.next = vertex;
+
+		}
+
+		vertex.prev = this.tail;
+		vertex.next = null; // the tail has no subsequent vertex
+
+		this.tail = vertex;
+
+		return this;
+
+	}
+
+	appendChain( vertex ) {
+
+		if ( this.head === null ) {
+
+			this.head = vertex;
+
+		} else {
+
+			this.tail.next = vertex;
+
+		}
+
+		vertex.prev = this.tail;
+
+		while ( vertex.next !== null ) {
+
+			vertex = vertex.next;
+
+		}
+
+		this.tail = vertex;
+
+		return this;
+
+	}
+
+	remove( vertex ) {
+
+		if ( vertex.prev === null ) {
+
+			this.head = vertex.next;
+
+		} else {
+
+			vertex.prev.next = vertex.next;
+
+		}
+
+		if ( vertex.next === null ) {
+
+			this.tail = vertex.prev;
+
+		} else {
+
+			vertex.next.prev = vertex.prev;
+
+		}
+
+		return this;
+
+	}
+
+	removeSubList( a, b ) {
+
+		if ( a.prev === null ) {
+
+			this.head = b.next;
+
+		} else {
+
+			a.prev.next = b.next;
+
+		}
+
+		if ( b.next === null ) {
+
+			this.tail = a.prev;
+
+		} else {
+
+			b.next.prev = a.prev;
+
+		}
+
+		return this;
+
+	}
+
+	empty() {
+
+		return this.head === null;
+
+	}
+
+}
+
+export { ConvexHull };
