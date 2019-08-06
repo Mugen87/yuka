f76027eb885cd5c73405407bc34efca2f6fadb12
Mugen87 <michael.herzog@human-interactive.org>
2019-08-06 12:38:01 +0200

    Update builds.

--- build/yuka.js
+++ build/yuka.js
@@ -5862,6 +5862,33 @@
 		}
 
 		/**
+		* Performs a ray/BVH intersection test and stores the intersection point
+		* to the given 3D vector. If no intersection is detected, *null* is returned.
+		*
+		* @param {BVH} bvh - A BVH.
+		* @param {Vector3} result - The result vector.
+		* @return {Vector3} The result vector.
+		*/
+		intersectBVH( bvh, result ) {
+
+			return bvh.root.intersectRay( this, result );
+
+		}
+
+		/**
+		* Performs a ray/BVH intersection test. Returns either true or false if
+		* there is a intersection or not.
+		*
+		* @param {BVH} bvh - A BVH.
+		* @return {boolean} Whether there is an intersection or not.
+		*/
+		intersectsBVH( bvh ) {
+
+			return bvh.root.intersectsRay( this );
+
+		}
+
+		/**
 		* Transforms this ray by the given 4x4 matrix.
 		*
 		* @param {Matrix4} m - The 4x4 matrix.
@@ -8732,6 +8759,42 @@
 			}
 
 			return null;
+
+		}
+
+		/**
+		 * Returns a new geometry without containing indices.
+		 *
+		 * @return {MeshGeometry} The new geometry.
+		 */
+		toTriangleSoup() {
+
+			const indices = this.indices;
+			const vertices = this.vertices;
+			let newVertices;
+
+			if ( indices ) {
+
+				newVertices = new Float32Array( indices.length * 3 );
+
+				for ( let i = 0, l = indices.length; i < l; i ++ ) {
+
+					const a = indices[ i ];
+					const stride = 3;
+
+					newVertices[ i * stride ] = vertices[ a * stride ];
+					newVertices[ ( i * stride ) + 1 ] = vertices[ ( a * stride ) + 1 ];
+					newVertices[ ( i * stride ) + 2 ] = vertices[ ( a * stride ) + 2 ];
+
+				}
+
+			} else {
+
+				newVertices = new Float32Array( vertices );
+
+			}
+
+			return new MeshGeometry( newVertices );
 
 		}
 
@@ -13856,6 +13919,571 @@
 
 	}
 
+	const v1$3 = new Vector3();
+	const v2$1 = new Vector3();
+	const v3 = new Vector3();
+
+	const xAxis = new Vector3( 1, 0, 0 );
+	const yAxis = new Vector3( 0, 1, 0 );
+	const zAxis = new Vector3( 0, 0, 1 );
+
+	const triangle$1 = { a: new Vector3(), b: new Vector3(), c: new Vector3() };
+	const intersection = new Vector3();
+
+	/**
+	* Class representing a bounding volume hierarchy. The current implementation
+	* represents single BVH nodes as AABBs. It can handle arbitrary branching factors
+	* and subdivide the given geometry until a defined hierarchy depth has reached.
+	* Right now, the algorithm only performs splits along the cardinal axes.
+	*
+	* @author {@link https://github.com/robp94|robp94}
+	* @author {@link https://github.com/Mugen87|Mugen87}
+	*/
+	class BVH {
+
+		/**
+		* Constructs a new BVH.
+		*
+		* @param {Number} branchingFactor - The branching factor.
+		* @param {Number} depth - The maximum hierarchical depth.
+		* @param {Number} primitivesPerNode - The minimum amount of primitives per BVH node.
+		*/
+		constructor( branchingFactor = 2, primitivesPerNode = 1, depth = 10 ) {
+
+			/**
+			* The branching factor (how many nodes per level).
+			* @type Number
+			* @default 2
+			*/
+			this.branchingFactor = branchingFactor;
+
+			/**
+			* The minimum amount of primitives per BVH node.
+			* @type Number
+			* @default 10
+			*/
+			this.primitivesPerNode = primitivesPerNode;
+
+			/**
+			* The maximum hierarchical depth.
+			* @type Number
+			* @default 10
+			*/
+			this.depth = depth;
+
+			/**
+			* The root BVH node.
+			* @type BVHNode
+			* @default null
+			*/
+			this.root = null;
+
+		}
+
+		/**
+		* Computes a BVH for the given mesh geometry.
+		*
+		* @param {MeshGeometry} geometry - The mesh geometry.
+		* @return {BVH} A reference to this BVH.
+		*/
+		fromMeshGeometry( geometry ) {
+
+			this.root = new BVHNode();
+
+			// primitives
+
+			const nonIndexedGeometry = geometry.toTriangleSoup();
+			const vertices = nonIndexedGeometry.vertices;
+			this.root.primitives.push( ...vertices );
+
+			// centroids
+
+			const primitives = this.root.primitives;
+
+			for ( let i = 0, l = primitives.length; i < l; i += 9 ) {
+
+				v1$3.fromArray( primitives, i );
+				v2$1.fromArray( primitives, i + 3 );
+				v3.fromArray( primitives, i + 6 );
+
+				v1$3.add( v2$1 ).add( v3 ).divideScalar( 3 );
+
+				this.root.centroids.push( v1$3.x, v1$3.y, v1$3.z );
+
+			}
+
+			// build
+
+			this.root.build( this.branchingFactor, this.primitivesPerNode, this.depth, 1 );
+
+			return this;
+
+		}
+
+		/**
+		* Executes the given callback for each node of the BVH.
+		*
+		* @param {Function} callback - The callback to execute.
+		* @return {BVH} A reference to this BVH.
+		*/
+		traverse( callback ) {
+
+			this.root.traverse( callback );
+
+			return this;
+
+		}
+
+	}
+
+	class BVHNode {
+
+		/**
+		* Constructs a BVH node.
+		*/
+		constructor() {
+
+			/**
+			* The parent BVH node.
+			* @type BVHNode
+			* @default null
+			*/
+			this.parent = null;
+
+			/**
+			* The child BVH nodes.
+			* @type Array
+			*/
+			this.children = new Array();
+
+			/**
+			* The bounding volume of this BVH node.
+			* @type AABB
+			*/
+			this.boundingVolume = new AABB();
+
+			/**
+			* The primitives (triangles) of this BVH node.
+			* Only filled for leaf nodes.
+			* @type Array
+			*/
+			this.primitives = new Array();
+
+			/**
+			* The centroids of the node's triangles.
+			* Only filled for leaf nodes.
+			* @type Array
+			*/
+			this.centroids = new Array();
+
+		}
+
+		/**
+		* Returns true if this BVH node is a root node.
+		*
+		* @return {Boolean} Whether this BVH node is a root node or not.
+		*/
+		root() {
+
+			return this.parent === null;
+
+		}
+
+		/**
+		* Returns true if this BVH node is a leaf node.
+		*
+		* @return {Boolean} Whether this BVH node is a leaf node or not.
+		*/
+		leaf() {
+
+			return this.children.length === 0;
+
+		}
+
+		/**
+		* Returns the depth of this BVH node in its hierarchy.
+		*
+		* @return {Number} The hierarchical depth of this BVH node.
+		*/
+		getDepth() {
+
+			let depth = 0;
+
+			let parent = this.parent;
+
+			while ( parent !== null ) {
+
+				parent = parent.parent;
+				depth ++;
+
+			}
+
+			return depth;
+
+		}
+
+		/**
+		* Executes the given callback for this BVH node and its ancestors.
+		*
+		* @param {Function} callback - The callback to execute.
+		* @return {BVHNode} A reference to this BVH node.
+		*/
+		traverse( callback ) {
+
+			callback( this );
+
+			for ( let i = 0, l = this.children.length; i < l; i ++ ) {
+
+				 this.children[ i ].traverse( callback );
+
+			}
+
+			return this;
+
+		}
+
+		/**
+		* Builds this BVH node. That means the respective bounding volume
+		* is computed and the node's primitives are distributed under new child nodes.
+		* This only happens if the maximum hierarchical depth is not yet reached and
+		* the node does contain enough primitives required for a split.
+		*
+		* @param {Number} branchingFactor - The branching factor.
+		* @param {Number} primitivesPerNode - The minimum amount of primitives per BVH node.
+		* @param {Number} maxDepth - The maximum  hierarchical depth.
+		* @param {Number} currentDepth - The current hierarchical depth.
+		* @return {BVHNode} A reference to this BVH node.
+		*/
+		build( branchingFactor, primitivesPerNode, maxDepth, currentDepth ) {
+
+			this.computeBoundingVolume();
+
+			// check depth and primitive count
+
+			const primitiveCount = this.primitives.length / 9;
+			const newPrimitiveCount = Math.floor( primitiveCount / branchingFactor );
+
+			if ( ( currentDepth <= maxDepth ) && ( newPrimitiveCount >= primitivesPerNode ) ) {
+
+				// split (distribute primitives on new child BVH nodes)
+
+				this.split( branchingFactor );
+
+				// proceed with build on the next hierarchy level
+
+				for ( let i = 0; i < branchingFactor; i ++ ) {
+
+					this.children[ i ].build( branchingFactor, primitivesPerNode, maxDepth, currentDepth + 1 );
+
+				}
+
+			}
+
+			return this;
+
+		}
+
+		/**
+		* Computes the AABB for this BVH node.
+		*
+		* @return {BVHNode} A reference to this BVH node.
+		*/
+		computeBoundingVolume() {
+
+			const primitives = this.primitives;
+
+			const aabb = this.boundingVolume;
+
+			// compute AABB
+
+			aabb.min.set( Infinity, Infinity, Infinity );
+			aabb.max.set( - Infinity, - Infinity, - Infinity );
+
+			for ( let i = 0, l = primitives.length; i < l; i += 3 ) {
+
+				v1$3.x = primitives[ i ];
+				v1$3.y = primitives[ i + 1 ];
+				v1$3.z = primitives[ i + 2 ];
+
+				aabb.expand( v1$3 );
+
+			}
+
+			return this;
+
+		}
+
+		/**
+		* Computes the split axis. Right now, only the cardinal axes
+		* are potential split axes.
+		*
+		* @return {Vector3} The split axis.
+		*/
+		computeSplitAxis() {
+
+			let maxX, maxY, maxZ = maxY = maxX = - Infinity;
+			let minX, minY, minZ = minY = minX = Infinity;
+
+			const centroids = this.centroids;
+
+			for ( let i = 0, l = centroids.length; i < l; i += 3 ) {
+
+				const x = centroids[ i ];
+				const y = centroids[ i + 1 ];
+				const z = centroids[ i + 2 ];
+
+				if ( x > maxX ) {
+
+					maxX = x;
+
+				}
+
+				if ( y > maxY ) {
+
+					maxY = y;
+
+				}
+
+				if ( z > maxZ ) {
+
+					maxZ = z;
+
+				}
+
+				if ( x < minX ) {
+
+					minX = x;
+
+				}
+
+				if ( y < minY ) {
+
+					minY = y;
+
+				}
+
+				if ( z < minZ ) {
+
+					minZ = z;
+
+				}
+
+			}
+
+			const rangeX = maxX - minX;
+			const rangeY = maxY - minY;
+			const rangeZ = maxZ - minZ;
+
+			if ( rangeX > rangeY && rangeX > rangeZ ) {
+
+				return xAxis;
+
+			} else if ( rangeY > rangeZ ) {
+
+				return yAxis;
+
+			} else {
+
+				return zAxis;
+
+			}
+
+		}
+
+		/**
+		* Splits the node and distributes node's primitives over new child nodes.
+		*
+		* @param {Number} branchingFactor - The branching factor.
+		* @return {BVHNode} A reference to this BVH node.
+		*/
+		split( branchingFactor ) {
+
+			const centroids = this.centroids;
+			const primitives = this.primitives;
+
+			// create (empty) child BVH nodes
+
+			for ( let i = 0; i < branchingFactor; i ++ ) {
+
+				this.children[ i ] = new BVHNode();
+				this.children[ i ].parent = this;
+
+			}
+
+			// sort primitives along split axis
+
+			const axis = this.computeSplitAxis();
+			const sortedPrimitiveIndices = new Array();
+
+			for ( let i = 0, l = centroids.length; i < l; i += 3 ) {
+
+				v1$3.fromArray( centroids, i );
+
+				// the result from the dot product is our sort criterion.
+				// it represents the projection of the centroid on the split axis
+
+				const p = v1$3.dot( axis );
+				const primitiveIndex = i / 3;
+
+				sortedPrimitiveIndices.push( { index: primitiveIndex, p: p } );
+
+			}
+
+			sortedPrimitiveIndices.sort( sortPrimitives );
+
+			// distribute data
+
+			const primitveCount = sortedPrimitiveIndices.length;
+			const primitivesPerChild = Math.floor( primitveCount / branchingFactor );
+
+			var childIndex = 0;
+			var primitivesIndex = 0;
+
+			for ( let i = 0; i < primitveCount; i ++ ) {
+
+				// selected child
+
+				primitivesIndex ++;
+
+				// check if we try to add more primitives to a child than "primitivesPerChild" defines.
+				// move primitives to the next child
+
+				if ( primitivesIndex > primitivesPerChild ) {
+
+					// ensure "childIndex" does not overflow (meaning the last child takes all remaining primitives)
+
+					if ( childIndex < ( branchingFactor - 1 ) ) {
+
+						primitivesIndex = 1; // reset primitive index
+						childIndex ++; // raise child index
+
+					}
+
+				}
+
+				const child = this.children[ childIndex ];
+
+				// move data to the next level
+
+				// 1. primitives
+
+				const primitiveIndex = sortedPrimitiveIndices[ i ].index;
+				const stride = primitiveIndex * 9; // remember the "primitives" array holds raw vertex data defining triangles
+
+				v1$3.fromArray( primitives, stride );
+				v2$1.fromArray( primitives, stride + 3 );
+				v3.fromArray( primitives, stride + 6 );
+
+				child.primitives.push( v1$3.x, v1$3.y, v1$3.z );
+				child.primitives.push( v2$1.x, v2$1.y, v2$1.z );
+				child.primitives.push( v3.x, v3.y, v3.z );
+
+				// 2. centroid
+
+				v1$3.fromArray( centroids, primitiveIndex * 3 );
+
+				child.centroids.push( v1$3.x, v1$3.y, v1$3.z );
+
+			}
+
+			// remove centroids/primitives after split from this node
+
+			this.centroids.length = 0;
+			this.primitives.length = 0;
+
+			return this;
+
+		}
+
+		/**
+		* Performs a ray/BVH node intersection test and stores the intersection point
+		* to the given 3D vector. If no intersection is detected, *null* is returned.
+		*
+		* @param {Ray} ray - The ray.
+		* @param {Vector3} result - The result vector.
+		* @return {Vector3} The result vector.
+		*/
+		intersectRay( ray, result ) {
+
+			if ( ray.intersectAABB( this.boundingVolume, result ) !== null ) {
+
+				if ( this.leaf() ) {
+
+					return this._intersectRayPrimitives( ray, result );
+
+				} else {
+
+					for ( let i = 0, l = this.children.length; i < l; i ++ ) {
+
+						if ( this.children[ i ].intersectRay( ray, result ) !== null ) {
+
+							return result;
+
+						}
+
+					}
+
+					return null;
+
+				}
+
+			} else {
+
+				return null;
+
+			}
+
+		}
+
+		/**
+		* Performs a ray/BVH node intersection test. Returns either true or false if
+		* there is a intersection or not.
+		*
+		* @param {Ray} ray - The ray.
+		* @return {boolean} Whether there is an intersection or not.
+		*/
+		intersectsRay( ray ) {
+
+			return this.intersectRay( ray, intersection ) !== null;
+
+		}
+
+		//
+
+		_intersectRayPrimitives( ray, result ) {
+
+			const vertices = this.primitives;
+
+			for ( let i = 0, l = vertices.length; i < l; i += 9 ) {
+
+				// remember: we assume primitives are triangles
+
+				triangle$1.a.fromArray( vertices, i );
+				triangle$1.b.fromArray( vertices, i + 3 );
+				triangle$1.c.fromArray( vertices, i + 6 );
+
+				if ( ray.intersectTriangle( triangle$1, true, result ) !== null ) {
+
+					return result;
+
+				}
+
+			}
+
+			return null;
+
+		}
+
+	}
+
+	//
+
+	function sortPrimitives( a, b ) {
+
+		return a.p - b.p;
+
+	}
+
 	const p1 = new Vector3();
 	const p2 = new Vector3();
 
@@ -16117,10 +16745,10 @@
 	const AbsR = [[], [], []];
 	const t = [];
 
-	const xAxis = new Vector3();
-	const yAxis = new Vector3();
-	const zAxis = new Vector3();
-	const v1$3 = new Vector3();
+	const xAxis$1 = new Vector3();
+	const yAxis$1 = new Vector3();
+	const zAxis$1 = new Vector3();
+	const v1$4 = new Vector3();
 	const closestPoint$1 = new Vector3();
 
 	/**
@@ -16236,8 +16864,8 @@
 
 			const halfSizes = this.halfSizes;
 
-			v1$3.subVectors( point, this.center );
-			this.rotation.extractBasis( xAxis, yAxis, zAxis );
+			v1$4.subVectors( point, this.center );
+			this.rotation.extractBasis( xAxis$1, yAxis$1, zAxis$1 );
 
 			// start at the center position of the OBB
 
@@ -16245,14 +16873,14 @@
 
 			// project the target onto the OBB axes and walk towards that point
 
-			const x = MathUtils.clamp( v1$3.dot( xAxis ), - halfSizes.x, halfSizes.x );
-			result.add( xAxis.multiplyScalar( x ) );
+			const x = MathUtils.clamp( v1$4.dot( xAxis$1 ), - halfSizes.x, halfSizes.x );
+			result.add( xAxis$1.multiplyScalar( x ) );
 
-			const y = MathUtils.clamp( v1$3.dot( yAxis ), - halfSizes.y, halfSizes.y );
-			result.add( yAxis.multiplyScalar( y ) );
+			const y = MathUtils.clamp( v1$4.dot( yAxis$1 ), - halfSizes.y, halfSizes.y );
+			result.add( yAxis$1.multiplyScalar( y ) );
 
-			const z = MathUtils.clamp( v1$3.dot( zAxis ), - halfSizes.z, halfSizes.z );
-			result.add( zAxis.multiplyScalar( z ) );
+			const z = MathUtils.clamp( v1$4.dot( zAxis$1 ), - halfSizes.z, halfSizes.z );
+			result.add( zAxis$1.multiplyScalar( z ) );
 
 			return result;
 
@@ -16266,14 +16894,14 @@
 		*/
 		containsPoint( point ) {
 
-			v1$3.subVectors( point, this.center );
-			this.rotation.extractBasis( xAxis, yAxis, zAxis );
+			v1$4.subVectors( point, this.center );
+			this.rotation.extractBasis( xAxis$1, yAxis$1, zAxis$1 );
 
 			// project v1 onto each axis and check if these points lie inside the OBB
 
-			return Math.abs( v1$3.dot( xAxis ) ) <= this.halfSizes.x &&
-					Math.abs( v1$3.dot( yAxis ) ) <= this.halfSizes.y &&
-					Math.abs( v1$3.dot( zAxis ) ) <= this.halfSizes.z;
+			return Math.abs( v1$4.dot( xAxis$1 ) ) <= this.halfSizes.x &&
+					Math.abs( v1$4.dot( yAxis$1 ) ) <= this.halfSizes.y &&
+					Math.abs( v1$4.dot( zAxis$1 ) ) <= this.halfSizes.z;
 
 		}
 
@@ -16347,13 +16975,13 @@
 
 			// compute translation vector
 
-			v1$3.subVectors( b.c, a.c );
+			v1$4.subVectors( b.c, a.c );
 
 			// bring translation into a’s coordinate frame
 
-			t[ 0 ] = v1$3.dot( a.u[ 0 ] );
-			t[ 1 ] = v1$3.dot( a.u[ 1 ] );
-			t[ 2 ] = v1$3.dot( a.u[ 2 ] );
+			t[ 0 ] = v1$4.dot( a.u[ 0 ] );
+			t[ 1 ] = v1$4.dot( a.u[ 1 ] );
+			t[ 2 ] = v1$4.dot( a.u[ 2 ] );
 
 			// compute common subexpressions. Add in an epsilon term to
 			// counteract arithmetic errors when two edges are parallel and
@@ -16463,13 +17091,13 @@
 		*/
 		intersectsPlane( plane ) {
 
-			this.rotation.extractBasis( xAxis, yAxis, zAxis );
+			this.rotation.extractBasis( xAxis$1, yAxis$1, zAxis$1 );
 
 			// compute the projection interval radius of this OBB onto L(t) = this->center + t * p.normal;
 
-			const r = this.halfSizes.x * Math.abs( plane.normal.dot( xAxis ) ) +
-					this.halfSizes.y * Math.abs( plane.normal.dot( yAxis ) ) +
-					this.halfSizes.z * Math.abs( plane.normal.dot( zAxis ) );
+			const r = this.halfSizes.x * Math.abs( plane.normal.dot( xAxis$1 ) ) +
+					this.halfSizes.y * Math.abs( plane.normal.dot( yAxis$1 ) ) +
+					this.halfSizes.z * Math.abs( plane.normal.dot( zAxis$1 ) );
 
 			// compute distance of the OBB's center from the plane
 
@@ -19804,6 +20432,8 @@
 	exports.AlignmentBehavior = AlignmentBehavior;
 	exports.ArriveBehavior = ArriveBehavior;
 	exports.BFS = BFS;
+	exports.BVH = BVH;
+	exports.BVHNode = BVHNode;
 	exports.BoundingSphere = BoundingSphere;
 	exports.CHFace = Face;
 	exports.CHVertex = Vertex;
