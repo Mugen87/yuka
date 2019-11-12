5f109142d6adb26542db101c97d4a4e0b9ad829e
Mugen87 <michael.herzog@human-interactive.org>
2019-11-12 13:29:49 +0100

    Examples: Clean up.

--- examples/lib/dat.gui.module.js
+++ examples/lib/dat.gui.module.js
@@ -678,8 +678,9 @@ Object.defineProperty(Color.prototype, 'a', {
 });
 Object.defineProperty(Color.prototype, 'hex', {
   get: function get$$1() {
-    if (!(this.__state.space !== 'HEX')) {
-      this.__state.hex = ColorMath.rgb_to_hex(this.r, this.g, this.b);
+    if (this.__state.space !== 'HEX') {
+      this.__state.hex = math.rgb_to_hex(this.r, this.g, this.b);
+      this.__state.space = 'HEX';
     }
     return this.__state.hex;
   },
