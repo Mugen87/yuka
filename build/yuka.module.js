e4840390341ca827b176892f46f2ae8b9ac90e95
Mugen87 <michael.herzog@human-interactive.org>
2020-02-19 19:12:17 +0100

    Update BVH.

--- build/yuka.module.js
+++ build/yuka.module.js
@@ -8710,19 +8710,19 @@ class MeshGeometry {
 	}
 
 	/**
-	 * Returns a new geometry without containing indices.
+	 * Returns a new geometry without containing indices. If the geometry is already
+	 * non-indexed, the method performs no changes.
 	 *
-	 * @return {MeshGeometry} The new geometry.
+	 * @return {MeshGeometry} The new non-indexed geometry.
 	 */
 	toTriangleSoup() {
 
 		const indices = this.indices;
-		const vertices = this.vertices;
-		let newVertices;
 
 		if ( indices ) {
 
-			newVertices = new Float32Array( indices.length * 3 );
+			const vertices = this.vertices;
+			const newVertices = new Float32Array( indices.length * 3 );
 
 			for ( let i = 0, l = indices.length; i < l; i ++ ) {
 
@@ -8735,13 +8735,13 @@ class MeshGeometry {
 
 			}
 
+			return new MeshGeometry( newVertices );
+
 		} else {
 
-			newVertices = new Float32Array( vertices );
+			return this;
 
 		}
-
-		return new MeshGeometry( newVertices );
 
 	}
 
@@ -14053,8 +14053,9 @@ class BVH {
 
 		// primitives
 
-		const nonIndexedGeometry = geometry.toTriangleSoup();
-		const vertices = nonIndexedGeometry.vertices;
+		if ( geometry.indices !== null ) geometry = geometry.toTriangleSoup();
+
+		const vertices = geometry.vertices;
 
 		for ( let i = 0, l = vertices.length; i < l; i ++ ) {
 
