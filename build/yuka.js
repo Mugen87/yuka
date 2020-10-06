1765f206f69adffde69836978f87f4665cb62722
Mugen87 <michael.herzog@human-interactive.org>
2020-10-06 17:59:09 +0200

    Update builds.

--- build/yuka.js
+++ build/yuka.js
@@ -28,7 +28,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(global = global || self, factory(global.YUKA = {}));
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.YUKA = {}));
 }(this, (function (exports) { 'use strict';
 
 	/**
