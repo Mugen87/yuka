78472e8ee9d7f9b9d6e13f41c50077062f4971d0
Mugen87 <michael.herzog@human-interactive.org>
2021-01-03 14:26:15 +0100

    Update to 0.4.0.

--- build/yuka.module.js
+++ build/yuka.module.js
@@ -3045,12 +3045,6 @@ class GameEntity {
 
 	}
 
-	set uuid( uuid ) {
-
-		this._uuid = uuid;
-
-	}
-
 	/**
 	* Executed when this game entity is updated for the first time by its {@link EntityManager}.
 	*
@@ -3348,7 +3342,6 @@ class GameEntity {
 	*/
 	fromJSON( json ) {
 
-		this.uuid = json.uuid;
 		this.name = json.name;
 		this.active = json.active;
 		this.neighborhoodRadius = json.neighborhoodRadius;
@@ -3374,6 +3367,8 @@ class GameEntity {
 		this._cache.scale.fromArray( json._cache.scale );
 
 		this._started = json._started;
+
+		this._uuid = json.uuid;
 
 		return this;
 
@@ -9937,12 +9932,6 @@ class FuzzySet extends FuzzyTerm {
 
 	}
 
-	set uuid( uuid ) {
-
-		this._uuid = uuid;
-
-	}
-
 	/**
 	* Computes the degree of membership for the given value. Notice that this method
 	* does not set {@link FuzzySet#degreeOfMembership} since other classes use it in
@@ -10028,7 +10017,8 @@ class FuzzySet extends FuzzyTerm {
 		this.representativeValue = json.representativeValue;
 		this.left = json.left;
 		this.right = json.right;
-		this.uuid = json.uuid;
+
+		this._uuid = json.uuid;
 
 		return this;
 
