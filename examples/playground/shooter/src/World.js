d8978a70992cff2adaa27951626e87410909b54c
Christian Schiffler <c.schiffler@cyberspectrum.de>
2021-05-14 14:01:49 +0200

    It is `lookingSpeed`, not `lookSpeed`

--- examples/playground/shooter/src/World.js
+++ examples/playground/shooter/src/World.js
@@ -301,7 +301,7 @@ class World {
 		const player = this.player;
 
 		this.controls = new FirstPersonControls( player );
-		this.controls.lookSpeed = 2;
+		this.controls.lookingSpeed = 2;
 
 		const intro = this.ui.intro;
 		const crosshairs = this.ui.crosshairs;
